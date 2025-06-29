7f3c68db47cfb235ca788c75a974f7f270a91b27
Pavel Kutka <pavel.kutka@seyfor.com>
2025-06-29 18:57:30 +0200

    Rename SA and edit frontend header

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "frontend" {
-  name                     = "pkdemoappfesa01"
+  name                     = "pkdemoappnesa01"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
